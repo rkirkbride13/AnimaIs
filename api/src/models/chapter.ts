60522098645c6698dcc87ab4577b0554f51d94f3
Robbie Kirkbride <robbie_kirkbride@hotmail.co.uk>
2023-03-30 15:56:08 +0100

    Chapter model created and test passing

---
+++ api/src/models/chapter.ts
@@ -0,0 +1,19 @@
+import mongoose, { Schema, Document } from 'mongoose'
+
+export interface IChapter extends Document {
+  user_id: string;
+  title: string;
+  content: Array<string>;
+  image: object;
+}
+
+const ChapterSchema: Schema = new Schema({
+  user_id: { type: String, required: true} ,
+  title: { type: String, required: true },
+  content: { type: Array, default: [] },
+  image: { type: Object },
+});
+
+const Chapter = mongoose.model<IChapter>("Chapter", ChapterSchema);
+
+export default Chapter;
