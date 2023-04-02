d8ccb432141bbb2795fb8ba8a44a704516799237
Robbie Kirkbride <robbie_kirkbride@hotmail.co.uk>
2023-04-02 15:56:45 +0100

    Component test for chapter form

---
+++ frontend/src/components/ChapterForm/ChapterForm.cy.tsx
@@ -0,0 +1,25 @@
+import ChapterForm from "./ChapterForm";
+
+const navigate = () => {};
+
+describe("Chapter Form", () => {
+  it("sends a request when the form is submitted", () => {
+    let setChaptersMock = cy.stub();
+    cy.mount(
+      <ChapterForm
+        navigate={navigate}
+        token={"tokenMock"}
+        setChapters={setChaptersMock}
+      />
+    );
+
+    cy.intercept("POST", "/chapters", { message: "OK" }).as("saveChapter");
+
+    cy.get('[data-cy="title"]').type("The Dog");
+    cy.get('[data-cy="submit-chapter"]').click();
+
+    cy.wait("@saveChapter").then((interception) => {
+      expect(interception.response!.body.message).to.eq("OK");
+    });
+  });
+});
